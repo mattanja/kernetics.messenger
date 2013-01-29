782e686728e66297c2ebd31f71f0d25ab549d8da
Mattanja Kern <mattanja.kern@gmail.com>
2013-01-29 10:04:52 +0800

    init

---
+++ kernetics.messenger.web/Program.cs
@@ -0,0 +1,48 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kernetics.messenger.web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri = "http://localhost:8888";
+            Console.WriteLine(uri);
+            // initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
+            var host = new NancyHost(new Uri(uri));
+            host.Start();  // start hosting
+
+            var smtpServer = new Rnwood.SmtpServer.DefaultServer();
+            smtpServer.MessageReceived += smtpServer_MessageReceived;
+            smtpServer.Start();
+
+            Console.WriteLine("SMTP server state: " + (smtpServer.IsRunning ? "Running" : "Not running"));
+
+            //Under mono if you deamonize a process a Console.ReadLine with cause an EOF 
+            //so we need to block another way
+            if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+            {
+                // TODO Thread sleep looks stupid
+                while (true) Thread.Sleep(10000000);
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+
+            smtpServer.Stop();
+            host.Stop();  // stop hosting
+        }
+
+        static void smtpServer_MessageReceived(object sender, Rnwood.SmtpServer.MessageEventArgs e)
+        {
+            Console.WriteLine("Message received: " + e.Message.ToString());
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+﻿
+messenger
+=========
+
+messenger is intended to be a lightweight replacement for good old GNU mailman.
+(At the moment it is only a test project for NancyFX and Service Stack hosted on Linux/Mono.)
+
+The following issues will be addressed:
+* Simple setup, simple configuration and independent from the underlying mailing system
+* Receiving mail from members of a list will forward mail to all members of the list
+* Web API for list management and sending mail
+
+Out of scope (for the time being):
+* Mail archive
+* Spam filtering (list of accepted remote servers though, so spam filtering can be done on regular mail server)
