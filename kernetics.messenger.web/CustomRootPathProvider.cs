4a63c261b9a6d2299cdd367e7582b678c11b4086
Mattanja Kern <mattanja.kern@gmail.com>
2013-02-03 17:32:41 +0800

    Dynamically determine current root path of application (Debug/Release/bin)

--- kernetics.messenger.web/CustomRootPathProvider.cs
+++ kernetics.messenger.web/CustomRootPathProvider.cs
@@ -9,10 +9,36 @@ namespace kernetics.messenger.web
 {
     public class CustomRootPathProvider : IRootPathProvider
     {
+        private string rootPath;
+
         public string GetRootPath()
         {
-            // TODO make dynamic path (dependent on Debug/Release or config)
-            return @"C:\projects\kernetics.messenger\kernetics.messenger.web";
+            return this.RootPath;
+        }
+
+        public string RootPath
+        {
+            get
+            {
+                if (this.rootPath == null)
+                {
+                    var assemblyPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                    if (assemblyPath.EndsWith("Debug") || assemblyPath.EndsWith("Release"))
+                    {
+                        this.rootPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(assemblyPath, "../.."));
+                    }
+                    else if (assemblyPath.EndsWith("bin"))
+                    {
+                        this.rootPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(assemblyPath, ".."));
+                    }
+                    else
+                    {
+                        this.rootPath = assemblyPath;
+                    }
+                    Console.WriteLine("Determined root path: " + this.rootPath);
+                }
+                return this.rootPath;
+            }
         }
     }
 }
